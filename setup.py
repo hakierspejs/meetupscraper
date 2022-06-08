6480d4fa0fa8d263bcb0d327a8f3b7435b77592a
Jacek Wielemborek <d33tah@gmail.com>
2022-06-08 14:47:41 +0200

    Bump version to 0.9.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='meetupscraper',
-    version='0.8.2',
+    version='0.9.0',
     description=__doc__,
     url='http://github.com/hakierspejs/meetupscraper',
     author='Hakiespejs Łódź',
