134983f47c660405fe25abf2737536e3ceb0c57e
Piotr Myśliński <me@pmysl.dev>
2022-12-13 08:07:31 +0100

    Bump version to 0.9.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='meetupscraper',
-    version='0.9.1',
+    version='0.9.2',
     description=__doc__,
     url='http://github.com/hakierspejs/meetupscraper',
     author='Hakiespejs Łódź',
